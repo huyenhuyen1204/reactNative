1ff66af34b0e9b588add8cc777885ceea2b035a8
huyenhuyen1204 <nguyenhuyen98pm@egmail.com>
2019-02-14 23:55:18 +0700

    redux basic

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-/** @format */
+// /** @format */
 
-import {AppRegistry} from 'react-native';
-import Calculator from './src/component/calculator/caculalor';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import Calculator from './src/component/calculator/caculalor';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Calculator);
+// AppRegistry.registerComponent(appName, () => Calculator);
+
+// /** @format */
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+const {createStore} = require('redux');
+
+const defaultState  = {value : 0};
+
+/**
+ * Sd state, action de tinh toan state ms
+ */
+const reducer = (state = defaultState, action) => {
+    if(action.type === 'UP') {
+        return {value: state.value + 1};
+    }else if (action.type === 'DOWN'){
+        return {value : state.value - 1};
+    }
+    return state;
+}
+
+const store = createStore(reducer);
+/**
+ *  Action Up, Down
+ * How to tiep can gia tri state?
+ * Thay doi gia tri state?
+ */
+
+ // Chi dc get, k dc set
+ const mState = store.getState().value;
+ console.log(mState);
+
+//Thay doi va nhan vao 1 action = obj
+ store.dispatch({type : 'UP'});
+ store.dispatch({type : 'UP'});
+
+ console.log(store.getState().value);
+
+ /**
+  * Cau truc tao:
+  * 1. require
+  * 2. defaulState, reducer(gom state va action)
+  * 4. CreateStore (reducer)
+  */
+
--- src/component/calculator/caculalor.js
+++ src/component/calculator/caculalor.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 import {
-    Text, View, StyleSheet, TouchableOpacity, Image
+    Text, View, StyleSheet, TouchableOpacity, Image, TouchableHighlight
 } from 'react-native';
 
 export default class Caculator extends Component {
     constructor(props){
         super(props);
+
+    this.state = {
+        preInput : '',
+        nextInput : 0,
+        operator : null
     }
+    }
+
     render(){
         return (
             <View style={styles.container}>
@@ -17,83 +24,120 @@ export default class Caculator extends Component {
                 </View>
                 <View style= {styles.inputBackGround}>
                 <View style = {styles.row}>
-                    <Image 
+                <TouchableHighlight >
+                    <Image          
                         source = {require('../../img/7.png')}
                         style={styles.input}
                     />
+                </TouchableHighlight>
+             {/* <TouchableOpacity>
                     <Image 
                         source = {require('../../img/8.png')}
                         style={styles.input}
                     />
+            </TouchableOpacity>
+                    <TouchableOpacity>
                     <Image 
                         source = {require('../../img/9.png')}
                         style={styles.input}
                     />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
                     <Image 
                         source = {require('../../img/chia.png')}
                         style={styles.input}
                     />
+                    </TouchableOpacity>
                 </View>
+                
                 <View style = {styles.row}>
+                <TouchableOpacity>
                     <Image 
                         source = {require('../../img/4.png')}
                         style={styles.input}
                     />
+                </TouchableOpacity>
+                <TouchableOpacity>
                     <Image 
                         source = {require('../../img/5.png')}
                         style={styles.input}
                     />
+                </TouchableOpacity>
+                <TouchableOpacity>
                     <Image 
                         source = {require('../../img/6.png')}
                         style={styles.input}
                     />
+                </TouchableOpacity>
+                <TouchableOpacity>
                     <Image 
                         source = {require('../../img/nhan.png')}
                         style={styles.input}
                     />
+                </TouchableOpacity>
                 </View>
                 <View style = {styles.row}>
+                <TouchableOpacity>
                     <Image 
                         source = {require('../../img/1.png')}
                         style={styles.input}
                     />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
                     <Image 
                         source = {require('../../img/2.png')}
                         style={styles.input}
                     />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
                     <Image 
                         source = {require('../../img/3.png')}
                         style={styles.input}
                     />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
                     <Image 
                         source = {require('../../img/tru.png')}
                         style={styles.input}
                     />
+                    </TouchableOpacity>
                 </View>
                 <View style = {styles.row}>
+                <TouchableOpacity>
                     <Image 
                         source = {require('../../img/0.png')}
                         style={styles.input}
                     />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
                     <Image 
                         source = {require('../../img/cham.png')}
                         style={styles.input}
                     />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
                     <Image 
                         source = {require('../../img/bang.png')}
                         style={styles.input}
                     />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
                     <Image 
                         source = {require('../../img/cong.png')}
                         style={styles.input}
                     />
+                    </TouchableOpacity> */}
                 </View>
                 </View>
             </View>
         );
     }
 }
-
+_onpress(value){
+    switch(value){
+        
+    }
+}
 var styles = StyleSheet.create({
     container: {
         flex : 1,
