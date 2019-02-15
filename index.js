defb7e9c6e91d92b7145e84df1d642130b6f8352
huyenhuyen1204 <nguyenhuyen98pm@egmail.com>
2019-02-15 08:36:24 +0700

    tich hop react-redux

--- index.js
+++ index.js
@@ -6,13 +6,14 @@
 
 // AppRegistry.registerComponent(appName, () => Calculator);
 
-// /** @format */
+/** @format */
+import React,{Component} from 'react';
+import { AppRegistry } from 'react-native';
+import App from './App';
+// Tich hop react-redux can provider
+import {Provider} from 'react-redux';
+import {name as appName} from './app.json';
 
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
 const {createStore} = require('redux');
 
 const defaultState  = {value : 0};
@@ -53,3 +54,14 @@ const store = createStore(reducer);
   * 4. CreateStore (reducer)
   */
 
+export default class DemoRedux extends Component {
+    render(){
+        return( 
+            <Provider store={store}>
+            <App />
+            </Provider>
+        );
+    }
+}
+
+AppRegistry.registerComponent(appName, () => App);
